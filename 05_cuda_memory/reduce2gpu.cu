9c07c9bd9ab14dfe4803dac8263ac5b487c6689d
ycao <1641395022@qq.com>
2026-08-21 09:54:13 +0800

    [feat] add reduce comment 1

--- 05_cuda_memory/reduce2gpu.cu
+++ 05_cuda_memory/reduce2gpu.cu
@@ -10,17 +10,33 @@ const int N = 1e8;
 const int M = sizeof(real) * N;
 const int BLOCK_SIZE = 128;
 
+// 这个归约算法称之为折半归约（binary reduction），
+// 他要求数组长度 N 必须被 BLOCK_SIZE 整除，且 BLOCK_SIZE 为 2 的整数次方，比如这里使用的 BLOCK_SIZE 为 128。
+// 具体做法是将数组分为 BLOCK_SIZE 部分，每部分交由一个线程块处理，
+// 对于每个 BLOCK_SIZE 子数组，将后半部分的各个元素与前半部分对应的数组元素相加，重复此过程，最后得到的第一个数组元素就是最初的数组中各个元素的和。
+// 这个过程要求对同一个线程块的线程进行同步，避免数据竞争。
+// 由于不同线程块负责不同的子数组，因此不需要进行线程同步。
 __global__ void reduce_global(real *d_x, real *d_y) {
     const int tid = threadIdx.x;
+    // 这句等价于：real *x = &d_x[blockDim.x * blockIdx.x];
+    // 他的作用是从整个数组中，取出 BLOCK_SIZE 个元素的首地址，交由一个线程块处理
     real *x = d_x + blockDim.x * blockIdx.x;
 
+    // blockDim.x >> 1 等价于 blockDim.x / 2; offset >>= 1 等价于 offset /= 2; 
+    // 使用位运算，是因为效率比较高。
     for (int offset = blockDim.x >> 1; offset > 0; offset >>= 1) {
+        // 这个判断确保随着归约的进行，越来越多的线程空闲下来
         if (tid < offset) {
             x[tid] += x[tid + offset];
         }
+        // 用于同步单个线程块里的线程，只能用于核函数（也包括被核函数调用的设备函数）
+        // 他的作用是保证一个线程块中的所有线程，在执行该语句后面的语句之前，完全执行了该语句前面的语句,
+        // 针对这个例子，该函数确保每次归约涉及的读和写都能顺利走完，不会被线程块的其他线程干扰，避免数据竞争（data race）。
         __syncthreads();
     }
 
+    // 这个函数归约的结果，是将 N 长的数组，变为 N/BLOCK_SIZE 长的数组，而不是直接归约为 1，请注意这一点
+    // 后续的逻辑会将 d_y 拷贝到主机端，使用循环完成最后的归约。
     if (tid == 0) {
         d_y[blockIdx.x] = x[0];
     }
@@ -31,7 +47,7 @@ __global__ void reduce_shared(real *d_x, real *d_y) {
     const int bid = blockIdx.x;
 
     const int n = bid * blockDim.x + tid;
-    __shared__ real s_y[128];
+    __shared__ real s_y[BLOCK_SIZE];
     s_y[tid] = (n < N) ? d_x[n] : 0.0;
     __syncthreads();
 
@@ -68,6 +84,8 @@ __global__ void reduce_dynamic(real *d_x, real *d_y) {
 }
 
 real reduce(real *d_x, const int method) {
+    // 等价于： (N % BLOCK_SIZE == 0) ? (N / BLOCK_SIZE) : (N / BLOCK_SIZE + 1);
+    // 也等价于：(N - 1) / BLOCK_SIZE + 1;
     int grid_size = (N + BLOCK_SIZE -1) / BLOCK_SIZE;
     const int ymem = sizeof(real) * grid_size;
     const int smem = sizeof(real) * BLOCK_SIZE;
@@ -93,6 +111,7 @@ real reduce(real *d_x, const int method) {
 
     CHECK_CUDA_CALL(cudaMemcpy(h_y, d_y, ymem, cudaMemcpyDeviceToHost));
 
+    // 在主机端完成最后的归约
     real result = 0.0;
     for (int i = 0; i < grid_size; ++i) {
         result += h_y[i];
@@ -130,6 +149,14 @@ void timing(real *h_x, real *d_x, const int method) {
     printf("sum = %f\n", sum);
 }
 
+
+// 数组归约：把数组中的所有元素，通过某个操作（如加法、求最大值），合并成一个值
+// CPU 归约：循环遍历
+// GPU 归约：把数组分成很多组，每组独立计算，最后再合并结果。每一轮线程数量减半，像树一样从叶子到根计算，时间复杂度从 O(N) 降到 O(log₂N)
+// 归约是 CUDA 编程的 Hello World，会同时用到共享内存，CUDA 线程同步，树形归约算法。理解了归约，就基本掌握了 CUDA 加速的核心思想，面试必考。
+
+// 共享内存是一种可被程序员直接操控的缓存，主要作用有两个：
+// 一个是减少核函数中对全局内存的访问次数，实现高效的线程块内部的通信，另一个是提高全局内存访问的合并度。
 int main(void) {
     real *h_x = (real *)malloc(M);
     for (int i = 0; i < N; ++i) {
