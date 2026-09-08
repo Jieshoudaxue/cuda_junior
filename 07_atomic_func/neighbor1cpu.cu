b49e1f74958af091fbfb8860615ac810bcdd1ef6
ycao <1641395022@qq.com>
2026-09-08 09:40:48 +0800

    [feat] up neighbor1cpu

--- 07_atomic_func/neighbor1cpu.cu
+++ 07_atomic_func/neighbor1cpu.cu
@@ -10,11 +10,17 @@
 // typedef float real;
 typedef double real;
 
-int N; // number of atoms
-const int NUM_REPEATS = 20;
-const int MAX_NUM_NEIGHBORS = 10; // maximum number of neighbors for each atom
-const real cutoff = 1.9; // in units of angtrom
+// 原子个数
+// 每个原子最大的邻居数量，这里设置为 3，实际应用中可能会更大
+int N;
+const int MAX_NUM_NEIGHBORS = 3;
+
+// 原子间的截断半径
+// 原子间的截断半径平方，如果两个原子间的距离平方小于 cutoff_square，则认为它们是邻居
+const real cutoff = 1.9; 
 const real cutoff_square = cutoff * cutoff;
+
+const int NUM_REPEATS = 20;
 
 void read_xy(std::vector<real> &xv, std::vector<real> &yv) {
     std::ifstream infile("/home/momenta/cuda_junior/07_atomic_func/xy.txt");
@@ -48,6 +54,7 @@ void read_xy(std::vector<real> &xv, std::vector<real> &yv) {
 }
 
 void find_neighbor(int *nn_ptr, int *nl_ptr, const real *x, const real *y) {
+    // 将所有原子的邻居个数初始化为零，方便后面累加
     for (int i = 0; i < N; i++) {
         nn_ptr[i] = 0;
     }
@@ -55,11 +62,15 @@ void find_neighbor(int *nn_ptr, int *nl_ptr, const real *x, const real *y) {
     for (int i = 0; i < N; i++) {
         real x1 = x[i];
         real y1 = y[i];
+        // 如果一个原子是另一个原子的邻居，那个反之也成立，
+        // 因此双层循环的内层不需要从 0 开始，只需要从 i + 1 开始，省去了一半的计算量
         for (int j = i + 1; j < N; j++) {
             real x12 = x[j] - x1;
             real y12 = y[j] - y1;
             real distance_square = x12 * x12 + y12 * y12;
             if (distance_square < cutoff_square) {
+                // nl_ptr[i * MAX_NUM_NEIGHBORS + k] 是第 i 个原子的第 k 个邻居的索引
+                // 这里的 nn_ptr[i] 是原子 i 的邻居数量，初始为 0，每找到一个邻居就加 1
                 nl_ptr[i * MAX_NUM_NEIGHBORS + nn_ptr[i]++] = j;
                 nl_ptr[j * MAX_NUM_NEIGHBORS + nn_ptr[j]++] = i;
             }
@@ -88,6 +99,8 @@ void timing(int *nn_ptr, int *nl_ptr, std::vector<real> &xv, std::vector<real> &
     }
 }
 
+// 打印每个原子的所有邻居原子的索引，注意中心原子本身不算邻居，不在邻居列表中，因此不打印
+// 但 neighbor.txt 文件的行号减一就是中心原子的索引，请读者知悉
 void print_neighbor(const int *nn_ptr, const int *nl_ptr) {
     std::ofstream outfile("/home/momenta/cuda_junior/07_atomic_func/neighbor.txt");
     if (!outfile) {
@@ -99,7 +112,6 @@ void print_neighbor(const int *nn_ptr, const int *nl_ptr) {
             std::cout << "Error: MAX_NUM_NEIGHBORS is too small" << std::endl;
             exit(1);
         }
-        outfile << nn_ptr[i];
         for (int j = 0; j < MAX_NUM_NEIGHBORS; j++) {
             if (j < nn_ptr[i]) {
                 outfile << " " << nl_ptr[i * MAX_NUM_NEIGHBORS + j];
@@ -113,13 +125,19 @@ void print_neighbor(const int *nn_ptr, const int *nl_ptr) {
 }
 
 int main(void) {
-    std::vector<real> x, y;
-    read_xy(x, y);
-    N = x.size();
+    // xy.txt 文件中存储了每个原子的坐标，第一列是 x 坐标，第二列是 y 坐标
+    // 读取 xy.txt 文件中的原子坐标，并存储在 x 和 y 向量中
+    // xv 的长度就是所有原子的个数，存储在全局变量 N 中，所有原子的索引范围是 [0, N-1]
+    std::vector<real> xv, yv;
+    read_xy(xv, yv);
+    N = xv.size();
+
+    // neighbor_number_ptr 用来存储每个原子的邻居数量，
+    // neighbor_list_ptr 用来存储每个原子的邻居列表，列表元素是邻居原子的索引
     int *neighbor_number_ptr = (int *)malloc(N * sizeof(int));
     int *neighbor_list_ptr = (int *)malloc(N * MAX_NUM_NEIGHBORS * sizeof(int));
 
-    timing(neighbor_number_ptr, neighbor_list_ptr, x, y);
+    timing(neighbor_number_ptr, neighbor_list_ptr, xv, yv);
     print_neighbor(neighbor_number_ptr, neighbor_list_ptr);
 
     free(neighbor_number_ptr);
