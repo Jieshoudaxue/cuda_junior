9958b5f720bb2d462be5761b97e2a5d62276d61a
ycao <1641395022@qq.com>
2026-09-09 09:30:55 +0800

    [feat] add neighbor gpu

--- 07_atomic_func/neighbor2gpu.cu
+++ 07_atomic_func/neighbor2gpu.cu
@@ -53,35 +53,34 @@ void read_xy(std::vector<real> &xv, std::vector<real> &yv) {
     infile.close();
 }
 
-__global__ void find_neighbor_atomic(int *d_nn_ptr, int *d_nl_ptr, const real *x, const real *y) {
+__global__ void find_neighbor(int *nn_ptr, int *nl_ptr, const real *x, const real *y, const int N) {
     const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    // 这里将 cpu 版本的外循环改为判断，基本就完成了 C++ 到 CUDA 的转换，
+    // 这种将 C++ 程序中的最外层循环改成核函数中的判断语句的做法是开发 CUDA 程序时经常用到的模式。
     if (i >= N) {
         return;
     }
-    d_nn_ptr[i] = 0;
+    // cpu 版本要想完成邻居个数初始化，需要一个循环，而 CUDA 版本就不需要了，一句赋值就行了。
+    // 这里本质还是 CPU 顺序执行与 GPU 并行执行的区别。
+    nn_ptr[i] = 0;
     
     real x1 = x[i];
     real y1 = y[i];
-    // 如果一个原子是另一个原子的邻居，那个反之也成立，
-    // 因此双层循环的内层不需要从 0 开始，只需要从 i + 1 开始，省去了一半的计算量
     for (int j = i + 1; j < N; j++) {
         real x12 = x[j] - x1;
         real y12 = y[j] - y1;
         real distance_square = x12 * x12 + y12 * y12;
         if (distance_square < cutoff_square) {
-            // nl_ptr[i * MAX_NUM_NEIGHBORS + k] 是第 i 个原子的第 k 个邻居的索引
-            // 这里的 nn_ptr[i] 是原子 i 的邻居数量，初始为 0，每找到一个邻居就加 1
-            nl_ptr[i * MAX_NUM_NEIGHBORS + atomicAdd(&d_nn_ptr[i], 1)] = j;
-            nl_ptr[j * MAX_NUM_NEIGHBORS + atomicAdd(&d_nn_ptr[j], 1)] = i;
+            // cpu 版本里，使用 nn_ptr[i]++ 和 nn_ptr[j]++ 来累加邻居个数，由于是 CPU 顺序执行，因此不会出现数据竞争问题。
+            // 但在 GPU 并行执行中，多个线程可能同时对同一个原子的邻居个数进行累加，这就会出现数据竞争问题，导致结果不正确。
+            // 解决数据竞争问题的办法是使用原子函数 atomicAdd()
+            nl_ptr[i * MAX_NUM_NEIGHBORS + atomicAdd(&nn_ptr[i], 1)] = j;
+            nl_ptr[j * MAX_NUM_NEIGHBORS + atomicAdd(&nn_ptr[j], 1)] = i;
         }
     }
 }
 
-__global__ void find_neighbor_no_atomic(int *d_nn_ptr, int *d_nl_ptr, const real *x, const real *y) {
-    // TODO
-}
-
-void timing(int *d_nn_ptr, int *d_nl_ptr, const real *d_xv, const real *d_yv, const bool use_atomic) {
+void timing(int *d_nn_ptr, int *d_nl_ptr, const real *d_xv, const real *d_yv) {
     for (int repeat = 0; repeat <= NUM_REPEATS; ++repeat) {
         cudaEvent_t start, stop;
         CHECK_CUDA_CALL(cudaEventCreate(&start));
@@ -91,11 +90,7 @@ void timing(int *d_nn_ptr, int *d_nl_ptr, const real *d_xv, const real *d_yv, co
 
         int block_size = 128;
         int grid_size = (N + block_size - 1)/block_size;
-        if (use_atomic) {
-            find_neighbor_atomic<<<grid_size, block_size>>>(d_nn_ptr, d_nl_ptr, d_xv, d_yv);
-        } else {
-            find_neighbor_no_atomic<<<grid_size, block_size>>>(d_nn_ptr, d_nl_ptr, d_xv, d_yv);
-        }
+        find_neighbor<<<grid_size, block_size>>>(d_nn_ptr, d_nl_ptr, d_xv, d_yv, N);
 
         CHECK_CUDA_CALL(cudaEventRecord(stop));
         CHECK_CUDA_CALL(cudaEventSynchronize(stop));
@@ -108,8 +103,6 @@ void timing(int *d_nn_ptr, int *d_nl_ptr, const real *d_xv, const real *d_yv, co
     }
 }
 
-// 打印每个原子的所有邻居原子的索引，注意中心原子本身不算邻居，不在邻居列表中，因此不打印
-// 但 neighbor.txt 文件的行号减一就是中心原子的索引，请读者知悉
 void print_neighbor(const int *nn_ptr, const int *nl_ptr) {
     std::ofstream outfile("/home/momenta/cuda_junior/07_atomic_func/neighbor.txt");
     if (!outfile) {
@@ -134,9 +127,6 @@ void print_neighbor(const int *nn_ptr, const int *nl_ptr) {
 }
 
 int main(void) {
-    // xy.txt 文件中存储了每个原子的坐标，第一列是 x 坐标，第二列是 y 坐标
-    // 读取 xy.txt 文件中的原子坐标，并存储在 x 和 y 向量中
-    // xv 的长度就是所有原子的个数，存储在全局变量 N 中，所有原子的索引范围是 [0, N-1]
     std::vector<real> xv, yv;
     read_xy(xv, yv);
     N = xv.size();
@@ -155,13 +145,11 @@ int main(void) {
     CHECK_CUDA_CALL(cudaMemcpy(d_yv, yv.data(), N * sizeof(real), cudaMemcpyHostToDevice));
 
     std::cout << std::endl << "using atomicAdd: " << std::endl;
-    timing(d_neighbor_num_ptr, d_neighbor_list_ptr, d_xv, d_yv, true);
-    std::cout << std::endl << "not using atomicAdd: " << std::endl;
-    timing(d_neighbor_num_ptr, d_neighbor_list_ptr, d_xv, d_yv, false);
+    timing(d_neighbor_num_ptr, d_neighbor_list_ptr, d_xv, d_yv);
 
     CHECK_CUDA_CALL(cudaMemcpy(h_neighbor_num_ptr, d_neighbor_num_ptr, N * sizeof(int), cudaMemcpyDeviceToHost));
     CHECK_CUDA_CALL(cudaMemcpy(h_neighbor_list_ptr, d_neighbor_list_ptr, N * MAX_NUM_NEIGHBORS * sizeof(int), cudaMemcpyDeviceToHost));
-    print_neighbor(h_neighbor_num_ptr, h_neighbor_list_ptr, false);
+    print_neighbor(h_neighbor_num_ptr, h_neighbor_list_ptr);
 
 
     CHECK_CUDA_CALL(cudaFree(d_xv));
