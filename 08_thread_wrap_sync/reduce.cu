b2bfd740f2373562fb7691e609dc7da4e6004d93
ycao <1641395022@qq.com>
2026-09-10 09:32:36 +0800

    [feat] add new reduce

--- 08_thread_wrap_sync/reduce.cu
+++ 08_thread_wrap_sync/reduce.cu
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdint>
+#include <cooperative_groups.h>
 
 #include "cuda_error.cuh"
 
@@ -12,8 +13,111 @@ const int M = sizeof(real) * N;
 const int BLOCK_SIZE = 128;
 const unsigned int FULL_MASK = 0xffffffff;
 
-real resuce(const real *d_x, const int method) {
-    // TODO
+__global__ void reduce_syncwarp(const real *d_x, real *d_y) {
+    const int tid = threadIdx.x;
+    const int n = blockIdx.x * blockDim.x + tid;
+
+    extern __shared__ real s_y[];
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
+    __syncthreads();
+
+    for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1) {
+        if (tid < offset) {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncthreads();
+    }
+
+    for (int offset = 16; offset > 0; offset >>= 1) {
+        if (tid < offset) {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncwarp();
+    }
+
+    if (tid == 0) {
+        atomicAdd(d_y, s_y[0]);
+    }
+}
+
+__global__ void reduce_shfl(const real *d_x, real *d_y) {
+    const int tid = threadIdx.x;
+    const int n = blockIdx.x * blockDim.x + tid;
+
+    extern __shared__ real s_y[];
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
+    __syncthreads();
+
+    for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1) {
+        if (tid < offset) {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncthreads();
+    }
+
+    real y = s_y[tid];
+    for (int offset = 16; offset > 0; offset >>= 1) {
+        y += __shfl_down_sync(FULL_MASK, y, offset);
+    }
+
+    if (tid == 0) {
+        atomicAdd(d_y, y);
+    }
+}
+
+__global__ void reduce_cooperative_group(const real *d_x, real *d_y) {
+    const int tid = threadIdx.x;
+    const int n = blockIdx.x * blockDim.x + tid;
+
+    extern __shared__ real s_y[];
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
+    __syncthreads();
+
+    for (int offset = blockDim.x >> 1; offset >= 32; offset >>= 1) {
+        if (tid < offset) {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncthreads();
+    }
+
+    real y = s_y[tid];
+    cooperative_groups::thread_block_tile<32> g = cooperative_groups::tiled_partition<32>(cooperative_groups::this_thread_block());
+    for (int i = g.size() >> 1; i > 0; i >>= 1) {
+        y += g.shfl_down(y, i);
+    }
+
+    if (tid == 0) {
+        atomicAdd(d_y, y);
+    }
+}
+
+real reduce(const real *d_x, const int method) {
+    const int grid_size = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    const int shared_mem_size = BLOCK_SIZE * sizeof(real);
+
+    real h_y[1] = {0};
+    real *d_y;
+    CHECK_CUDA_CALL(cudaMalloc((void **)&d_y, sizeof(real)));
+    CHECK_CUDA_CALL(cudaMemcpy(d_y, h_y, sizeof(real), cudaMemcpyHostToDevice));
+
+    switch (method) {
+        case 0:
+            reduce_syncwarp<<<grid_size, BLOCK_SIZE, shared_mem_size>>>(d_x, d_y);
+            break;
+        case 1:
+            reduce_shfl<<<grid_size, BLOCK_SIZE, shared_mem_size>>>(d_x, d_y);
+            break;
+        case 2:
+            reduce_cooperative_group<<<grid_size, BLOCK_SIZE, shared_mem_size>>>(d_x, d_y);
+            break;
+        default:
+            printf("Invalid method: %d\n", method);
+            break;
+    }
+
+    CHECK_CUDA_CALL(cudaMemcpy(h_y, d_y, sizeof(real), cudaMemcpyDeviceToHost));
+    CHECK_CUDA_CALL(cudaFree(d_y));
+    return h_y[0];
 }
 
 void timing(real *d_x, const int method) {
