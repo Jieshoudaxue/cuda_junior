d5b48056349afbac7b41f38856f498b2e4e61bc8
ycao <1641395022@qq.com>
2026-09-11 09:46:11 +0800

    [feat] add warp func

--- 08_thread_wrap_sync/warp_func.cu
+++ 08_thread_wrap_sync/warp_func.cu
@@ -11,7 +11,7 @@ __global__ void test_warp_primitives(void) {
     int lane_id = tid % WIDTH;
 
     if (tid == 0) {
-        printf("threadIdx.x: ");
+        printf("threadIdx.x:    ");
     }
     printf("%2d ", tid);
     if (tid == 0) {
@@ -19,7 +19,7 @@ __global__ void test_warp_primitives(void) {
     }
 
     if (tid == 0) {
-        printf("lane_id: ");
+        printf("lane_id:        ");
     }
     printf("%2d ", lane_id);
     if (tid == 0) {
@@ -38,12 +38,64 @@ __global__ void test_warp_primitives(void) {
         printf("mask2       = %x\n", mask2);
     }
 
-    // TODO
+    int result = __all_sync(FULL_MASK, tid);
+    if (tid == 0) {
+        printf("__all_sync(FULL_MASK): %d\n", result);
+    }
+    result = __all_sync(mask1, tid);
+    if (tid == 1) {
+        printf("__all_sync    (mask1): %d\n", result);
+    }
+
+    result = __any_sync(FULL_MASK, tid);
+    if (tid == 0) {
+        printf("__any_sync(FULL_MASK): %d\n", result);
+    }
+    result = __any_sync(mask2, tid);
+    if (tid == 0) {
+        printf("__any_sync    (mask2): %d\n", result);
+    }
+
+    int value = __shfl_sync(FULL_MASK, tid, 2, WIDTH);
+    if (tid == 0) {
+        printf("__shfl_sync:        ");
+    }
+    printf("%2d ", value);
+    if (tid == 0) {
+        printf("\n");
+    }
+
+    value = __shfl_up_sync(FULL_MASK, tid, 1, WIDTH);
+    if (tid == 0) {
+        printf("__shfl_up_sync:     ");
+    }
+    printf("%2d ", value);
+    if (tid == 0) {
+        printf("\n");
+    }
+
+    value = __shfl_down_sync(FULL_MASK, tid, 1, WIDTH);
+    if (tid == 0) {
+        printf("__shfl_down_sync:   ");
+    }
+    printf("%2d ", value);
+    if (tid == 0) {
+        printf("\n");
+    }
+
+    value = __shfl_xor_sync(FULL_MASK, tid, 1, WIDTH);
+    if (tid == 0) {
+        printf("__shfl_xor_sync:    ");
+    }
+    printf("%2d ", value);
+    if (tid == 0) {
+        printf("\n");
+    }
 }
 
 int main(void) {
     test_warp_primitives<<<1, BLOCK_SIZE>>>();
-    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+    CHECK_CUDA_CALL(cudaDeviceSynchronize());
 
     return 0;
 }
